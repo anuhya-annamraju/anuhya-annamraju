ca4d9630ac296590794b30542a2c73f6ab7c41d9
Anuhya Annamraju <AA@airrobot.de>
2022-04-12 15:26:15 +0200

    refactor : removing reduntant code and assigning sensible variables

--- Python/Matplotlib_Demo/figures_and_axes.py
+++ Python/Matplotlib_Demo/figures_and_axes.py
@@ -10,42 +10,58 @@ matplotlib figures and subplots inside figures.
 
 from matplotlib import pyplot as plt
 
+data_x = [1,2,3,4,5,6,7,8,9]
+data_y = [1,2,3,4,5,6,7,8,9]
+
 #Close previous figures if any is open
 plt.close('all')
 
 #Single Figure with No Axis
 fig = plt.figure()
-plt.plot([1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9])
+plt.plot(data_x,data_y)
 plt.title("Single Figure Without Axes")
 
 #Add Axis to Single Figure with No Axis
 fig = plt.figure()
 ax = plt.subplot()
-ax.plot([1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9])
+ax.plot(data_x,data_y)
 plt.title("Adding Axis to Single Figure Without Axes")
 
 #Single figure with single axis
 fig, ax = plt.subplots()
-ax.plot([1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9])
+ax.plot(data_x,data_y)
 plt.title("Single Figure With Single Axis")
 
 
 #Single figure with single axis and multiple plots
 fig, ax = plt.subplots()
-ax.plot([1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9])
-ax.plot([1,2,3,4,5,6,7,8,9],[1,23,32,43,47,58,6,77,28])
-ax.plot([1,2,3,4,5,6,7,8,9],[11,3,100,53,49,82,16,107,128])
+ax.plot(data_x,data_y)
+data_y = [1,23,32,43,47,58,6,77,28]
+ax.plot(data_x,data_y)
+data_y = [11,3,100,53,49,82,16,107,128]
+ax.plot(data_x,data_y)
 plt.title("Single Figure With Single Axis And Multiple Plots")
 
 
 #Single figure with multiple axes
 fig, axes = plt.subplots(2,2)
 # axes is a multidimensional numpy array
-axes[0][0].plot([1,2,3,4,5,6,7,8,9],[1,23,32,43,47,58,6,77,28])
-axes[0][0].set_title("Title of Axes[0][0]")
-axes[0][1].plot([1,2,3,4,5,6,7,8,9],[11,3,100,53,49,82,16,107,128])
-axes[0][1].set_title("Title of Axes[0][1]")
-axes[1][0].plot([1,2,3,4,5,6,7,8,9],[156,38,12,93,37,48,86,27,238])
-axes[1][0].set_title("Title of Axes[1][0]")
-axes[1][1].plot([1,2,3,4,5,6,7,8,9],[14,243,322,413,57,89,49,71,98])
-axes[1][1].set_title("Title of Axes[1][1]")
+data_y = [1,23,32,43,47,58,6,77,28]
+ax = axes[0][0]
+ax.plot(data_x,data_y)
+ax.set_title("Title of Axes[0][0]")
+
+data_y = [11,3,100,53,49,82,16,107,128]
+ax = axes[0][1]
+ax.plot(data_x,)
+ax.set_title("Title of Axes[0][1]")
+
+data_y = [156,38,12,93,37,48,86,27,238]
+ax = axes[1][0]
+ax.plot(data_x,data_y)
+ax.set_title("Title of Axes[1][0]")
+
+data_y = [14,243,322,413,57,89,49,71,98]
+ax = axes[1][1]
+ax.plot(data_x,data_y)
+ax.set_title("Title of Axes[1][1]")
--- Python/Matplotlib_Demo/scatter_and_line.py
+++ Python/Matplotlib_Demo/scatter_and_line.py
@@ -11,15 +11,28 @@ from matplotlib import pyplot as plt
 
 #Close previous figures if any is open
 plt.close('all')
+data_x = [1,2,3,4,5,6,7,8,9]
 
 #Line Plots and Scatter Plots
 fig, axes = plt.subplots(2,2)
 # axes is a multidimensional numpy array
-axes[0][0].plot([1,2,3,4,5,6,7,8,9],[1,23,32,43,47,58,6,77,28],marker = 'x')
-axes[0][0].set_title("Plot with Marker x")
-axes[0][1].scatter([1,2,3,4,5,6,7,8,9],[11,3,100,53,49,82,16,107,128])
-axes[0][1].set_title("Scatter without Marker")
-axes[1][0].scatter([1,2,3,4,5,6,7,8,9],[156,38,12,93,37,48,86,27,238], marker = 'x')
-axes[1][0].set_title("Scatter with Marker x")
-axes[1][1].plot([1,2,3,4,5,6,7,8,9],[14,243,322,413,57,89,49,71,98])
-axes[1][1].set_title("Plot without Marker")
+ax = axes[0][0]
+data_y = [1,23,32,43,47,58,6,77,28]
+ax.plot(data_x,data_y,marker = 'x')
+ax.set_title("Plot with Marker x")
+
+
+ax = axes[0][1]
+data_y = [11,3,100,53,49,82,16,107,128]
+ax.scatter(data_x,data_y)
+ax.set_title("Scatter without Marker")
+
+ax = axes[1][0]
+data_y = [156,38,12,93,37,48,86,27,238]
+ax.scatter(data_x,data_y, marker = 'x')
+ax.set_title("Scatter with Marker x")
+
+ax = axes[1][1]
+data_y = [14,243,322,413,57,89,49,71,98]
+ax.plot(data_x,data_y)
+ax.set_title("Plot without Marker")
